e048ec1a0f113c21edbc1e9fd58b8d3158f83b36
Nicola Corna <nicola@corna.info>
2016-02-25 13:23:21 +0100

    First commit

---
+++ RGBFader.cpp
@@ -0,0 +1,141 @@
+/*
+ * RGB Fader
+ * Copyright (C) 2016 Nicola Corna <nicola@corna.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "RGBFader.h"
+
+const RGB RGBFader::rainbow[] = { {255, 0,   0   },
+                                  {255, 127, 0   },
+                                  {255, 255, 0   },
+                                  {0,   255, 0   },
+                                  {0,   0,   255 },
+                                  {75,  0,   130 },
+                                  {139, 0,   255 } };
+
+const uint8_t RGBFader::rainbowSize = 7;
+
+const RGB RGBFader::redGreenBlue[] = { {255, 0,   0   },
+                                       {0,   255, 0   },
+                                       {0,   0,   255 } };
+
+const uint8_t RGBFader::redGreenBlueSize = 3;
+
+const uint8_t RGBFader::exponentialLookup[] PROGMEM =
+{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+  0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x02, 0x02,
+  0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03,
+  0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x05,
+  0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x07, 0x07, 0x07,
+  0x07, 0x07, 0x07, 0x08, 0x08, 0x08, 0x08, 0x08, 0x09, 0x09, 0x09, 0x09, 0x09, 0x0A, 0x0A, 0x0A,
+  0x0A, 0x0B, 0x0B, 0x0B, 0x0B, 0x0C, 0x0C, 0x0C, 0x0D, 0x0D, 0x0D, 0x0E, 0x0E, 0x0E, 0x0E, 0x0F,
+  0x0F, 0x10, 0x10, 0x10, 0x11, 0x11, 0x11, 0x12, 0x12, 0x13, 0x13, 0x14, 0x14, 0x14, 0x15, 0x15,
+  0x16, 0x16, 0x17, 0x17, 0x18, 0x19, 0x19, 0x1A, 0x1A, 0x1B, 0x1B, 0x1C, 0x1D, 0x1D, 0x1E, 0x1F,
+  0x1F, 0x20, 0x21, 0x22, 0x22, 0x23, 0x24, 0x25, 0x26, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C,
+  0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x36, 0x37, 0x38, 0x39, 0x3B, 0x3C, 0x3D, 0x3F,
+  0x40, 0x41, 0x43, 0x44, 0x46, 0x48, 0x49, 0x4B, 0x4C, 0x4E, 0x50, 0x52, 0x53, 0x55, 0x57, 0x59,
+  0x5B, 0x5D, 0x5F, 0x61, 0x63, 0x66, 0x68, 0x6A, 0x6D, 0x6F, 0x72, 0x74, 0x77, 0x79, 0x7C, 0x7F,
+  0x81, 0x84, 0x87, 0x8A, 0x8D, 0x90, 0x94, 0x97, 0x9A, 0x9E, 0xA1, 0xA5, 0xA8, 0xAC, 0xB0, 0xB4,
+  0xB8, 0xBC, 0xC0, 0xC4, 0xC9, 0xCD, 0xD1, 0xD6, 0xDB, 0xE0, 0xE5, 0xEA, 0xEF, 0xF4, 0xF9, 0xFF };
+
+RGBFader::RGBFader(const RGB& rgb_pins, const RGB colors[], const uint8_t colorsNum, const uint8_t color_steps, const uint8_t brightness_speed,
+                   const uint8_t initialBrightness, const bool colorCycle, curveType curve) :
+  colorCycle(colorCycle),
+  freezeColor(false),
+  freezeBrightness(false),
+  pins(rgb_pins),
+  color_steps(color_steps),
+  brightness_speed(brightness_speed),
+  curveFunction(curve == LINEAR ? linear : (curve == QUADRATIC ? quadratic : exponential)),
+  brightness(initialBrightness),
+  brightnessTarget(initialBrightness),
+  cycleBrightnessTarget(initialBrightness),
+  cycleBrightness(false),
+  finalPauseCycles(0)
+{
+  setColors(colors, colorsNum);
+}
+
+void RGBFader::setColorsOffset(const RGB colors[], const uint8_t colorsNum, uint8_t offset) {
+
+  for (uint8_t i = 0; i < colorsNum; i++)
+    this->colors[i + offset] = colors[i];
+  
+  this->colorsNum = colorsNum + offset;
+  prevColor = this->colors;
+  nextColor = (prevColor - this->colors + 1) % colorsNum + this->colors;
+  position = 0;
+  color = colors[0];
+  colorEnded = false;
+
+}
+
+void RGBFader::nextStep() {
+  bool changed = false;
+  
+  if ((!colorEnded || colorCycle) && !freezeColor) {   
+    color.red = map(position, 0, color_steps, prevColor->red, nextColor->red);
+    color.green = map(position, 0, color_steps, prevColor->green, nextColor->green);
+    color.blue = map(position, 0, color_steps, prevColor->blue, nextColor->blue);
+  
+    if (position == color_steps - 1 && nextColor == colors + colorsNum - 1)
+      colorEnded = true;
+    else
+      colorEnded = false;
+
+    position = (position + 1) % color_steps;
+    if (position == 0) {
+      prevColor = nextColor;
+      nextColor = (nextColor - colors + 1) % colorsNum + colors;
+    }
+
+    changed = true;
+  }
+
+  if (!freezeBrightness) {
+    if (brightness != brightnessTarget) {
+      int8_t step;
+      
+      if (brightness > brightnessTarget)
+        step = -min(brightness - brightnessTarget, brightness_speed);
+      else
+        step = min(brightnessTarget - brightness, brightness_speed);
+  
+      brightness += step;
+  
+      changed = true;
+    }
+  
+    if (cycleBrightness && brightness == brightnessTarget) {
+      static uint8_t i = 0;
+
+      if (i >= finalPauseCycles) {
+        uint8_t temp = brightnessTarget;
+        brightnessTarget = cycleBrightnessTarget;
+        cycleBrightnessTarget = temp;
+        i = 0;
+      }
+      else
+        ++i;
+    }
+  }
+
+  if (changed)
+    applyChanges();
+}
+
---
+++ RGBFader.h
@@ -0,0 +1,141 @@
+/*
+ * RGB Fader
+ * Copyright (C) 2016 Nicola Corna <nicola@corna.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <Arduino.h>
+
+#define MAX_COLORS 16
+
+class RGB {
+public:
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+
+  RGB(uint8_t red, uint8_t green, uint8_t blue) : red(red), green(green), blue(blue) {}
+  RGB(const RGB& copy) : red(copy.red), green(copy.green), blue(copy.blue) {}
+  RGB() : RGB(0, 0, 0) {}
+};
+
+class RGBFader {
+public:
+  enum curveType {
+    LINEAR,
+    QUADRATIC,
+    EXPONENTIAL
+  };
+
+  static const RGB rainbow[];
+  static const uint8_t rainbowSize;
+  static const RGB redGreenBlue[];
+  static const uint8_t redGreenBlueSize;
+  
+  static const uint8_t exponentialLookup[256];
+  
+  RGBFader(const RGB& rgb_pins, const RGB colors[], const uint8_t colorsNum, const uint8_t color_steps = 64, const uint8_t brightness_speed = 4, 
+           const uint8_t initialBrightness = 255, const bool colorCycle = false, curveType curve = EXPONENTIAL);
+  void nextStep();
+  
+  static inline void linear(int pin, uint8_t value) {
+    analogWrite(pin, value);
+  }
+
+  static inline void quadratic(int pin, uint8_t value) {
+    analogWrite(pin, map((uint16_t)(value)*value, 0, 65025, 0, 255));
+  }
+
+  static inline void exponential(int pin, uint8_t value) {
+    analogWrite(pin, pgm_read_byte_near(exponentialLookup + value));
+  }
+
+/*
+  static inline void exponential(int pin, uint8_t value) {
+    analogWrite(pin, round(pow(2, value / 31.875)) - 1);
+  }
+*/
+
+  inline void setColors(const RGB colors[], const uint8_t colorsNum) {
+    setColorsOffset(colors, colorsNum, 0);
+  }
+
+  inline void setColors(const RGB& colors) {
+    setColorsOffset(&colors, 1, 0);
+  }
+  
+  inline void setNextColors(const RGB colors[], const uint8_t colorsNum) {
+    this->colors[0] = color;
+    setColorsOffset(colors, colorsNum, 1);
+  }
+
+  inline void setNextColors(const RGB& colors) {
+    this->colors[0] = color;
+    setColorsOffset(&colors, 1, 1);
+  }
+
+  inline RGB getColor() {
+    return color;
+  }
+
+  inline void setBrightness(uint8_t brightnessTarget) {
+    this->brightnessTarget = brightnessTarget;
+    cycleBrightness = false;
+  }
+
+  inline void setBrightnessCycle(uint8_t brightnessTarget1, uint8_t brightnessTarget2, uint8_t finalPauseCycles = 0) {
+    brightnessTarget = brightnessTarget1;
+    cycleBrightnessTarget = brightnessTarget2;
+    cycleBrightness = true;
+    this->finalPauseCycles = finalPauseCycles;
+  }
+
+  inline uint8_t getBrightness() {
+    return brightness;
+  }
+
+  bool colorCycle;
+  bool freezeColor;
+  bool freezeBrightness;
+
+protected:
+  const RGB pins;
+  const uint8_t color_steps;
+  const uint8_t brightness_speed;
+  void (*const curveFunction)(int, uint8_t);
+  
+  const RGB *prevColor;
+  const RGB *nextColor;
+
+  RGB colors[MAX_COLORS];
+  uint8_t colorsNum;
+  RGB color;
+  uint8_t brightness;
+  uint8_t brightnessTarget;
+  uint8_t cycleBrightnessTarget;
+  bool cycleBrightness;
+  uint8_t finalPauseCycles;
+  uint8_t position;
+  bool colorEnded;
+
+  void setColorsOffset(const RGB colors[], const uint8_t colorsNum, uint8_t offset);
+
+  inline void applyChanges() {
+    curveFunction(pins.red, map(color.red, 0, 255, 0, brightness));
+    curveFunction(pins.green, map(color.green, 0, 255, 0, brightness));
+    curveFunction(pins.blue, map(color.blue, 0, 255, 0, brightness));
+  }
+};
